c15f179b2fe7e47d0c70864d07f49652670e86f4
Christian M. Macy <christian@sparrowcompass.com>
2023-05-12 21:11:49 -0700

    Add simple form example with tailwind active

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,93 @@
-module Main exposing (Msg(..), main, update, view)
+module Main exposing (Model, Msg(..), init, main, update, view, viewInput, viewValidation)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
 
 
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
+
+
+init : Model
+init =
+    Model "" "" ""
+
+
+
+-- UPDATE
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Name String
+    | Password String
+    | PasswordAgain String
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        Name name ->
+            { model | name = name }
 
-        Decrement ->
-            model - 1
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
 
 
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-    div []
-        [ button [ class "btn-increment", onClick Decrement ] [ text "-" ]
-        , div [ class "count" ] [ text (String.fromInt model) ]
-        , button [ class "btn-decrement", onClick Increment ] [ text "+" ]
+    div [ class "container m-12 p-12" ]
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model
         ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    div [ class "sm:col-span-4" ]
+        [ label [ for t, class "block text-sm font-medium leading-6 text-gray-900" ] [ text (String.toUpper p) ]
+        , input
+            [ type_ t
+            , name t
+            , id t
+            , class "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            , placeholder p
+            , value v
+            , onInput toMsg
+            ]
+            []
+        ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    if model.password == model.passwordAgain then
+        div [ class "text-green-500" ] [ text "OK" ]
+
+    else
+        div [ class "text-red-800" ] [ text "Passwords do not match!" ]
